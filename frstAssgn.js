91031f2a833d758510b4c71a3e3b6e19546274fb
munguriek <benjaminmunguriek@gmail.com>
2020-09-24 21:22:16 +0300

    Solution to the first assignment

--- frstAssgn.js
+++ frstAssgn.js
@@ -1,54 +1,56 @@
-//
+//declare a variable name
 var name;
-//
-name = " ozzy zainab"
+//Assign value to a variable name
+name = " ozzy zainab";
 
-//
+//Printout name on the console
 console.log(name);
-// 
+// Printout <br /> on the console
 console.log("<br/>");
-//
+//Declare a variable name
 
 var name;
-//
-name = "ozzy emma otim "
-//
-console.log(name)
-//
+//Assign variable name
+name = "ozzy emma otim ";
 
+//Printout variable ozzy emma otim on the console
+console.log(name);
+
+//delcare variable name
 var name;
-//
-name = "marrie jacob ozyy"
-//
-console.log(name)
-//
+//assign value to name variable
+name = "marrie jacob ozyy";
+//Printout value of name to the console
+console.log(name);
+//declare variable name
 var name;
-//
-name = " ozzy diane"
-//
-console.log(name)
-//
-name = "joanita paul ozzy "
-//
-console.log(name)
-//
+//assign string  ozzy diane to name
+name = " ozzy diane";
+//print out the value of variable name to the console
+console.log(name);
+//reassign new value to variable name
+name = "joanita paul ozzy ";
+//printout the new variable value
+console.log(name);
+//declare a variable age
 var age;
-//
+//declare a variable name
 var name;
-//
+//assign an int value 50 to age variable
 age = 50;
-//
-name = "rino monica nicholus"
+//assign a string value rino monica nicholus to variable
+name = "rino monica nicholus";
 /*
-
+printout the My name is rino monica nicholus and I am 50 years old in a html page
+the document.write() function errases all contents of the html page
 */
 document.write("My name is " +name +" "+ "and I am "+age+" "+" years old");
 
-//
+//declare a variable x
 var x;
-//
+//print out variable x which will be undefined because it has not been initialised 
 document.write(x);
-//
+//initialise x to undefined
 var x = undefined;
-//
+//print out the value of x
 console.log(x);
