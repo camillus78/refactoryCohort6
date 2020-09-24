ab5b60c79be2b2d1aba8e1db62b2fa1a478b8433
Martin Anderson <kaweesimartinanderson@gmail.com>
2020-09-24 23:24:30 +0300

    Kaweesi Martin Anderson answers.

---
+++ MartinAnderson.js
@@ -0,0 +1,55 @@
+//Declared a variable name.
+var name;
+//intialised name variable to ozzy zainab.
+name = " ozzy zainab";
+
+//Output ozzy zainab in the console.
+console.log(name);
+// break the current line to the next. 
+console.log("<br/>");
+//Redeclare the name variable.
+
+var name;
+//Reinitialise name variable to ozzy emma otim.
+name = "ozzy emma otim ";
+
+//Output ozzy emma otim in the console
+console.log(name);
+
+//Redeclare the name variable.
+var name;
+//Reinitialise name variable to marrie jacob ozyy.
+name = "marrie jacob ozyy";
+//Output marrie jacob ozyy in the console.
+console.log(name);
+//Redeclare the name variable.
+var name;
+//Reinitialise name variable to ozzy diane.
+name = " ozzy diane";
+//Output ozzy diane in the console.
+console.log(name);
+//Reinitialise name variable to joanita paul ozzy .
+name = "joanita paul ozzy ";
+//Output joanita paul ozzy in the console.
+console.log(name);
+//Declared a variable age.
+var age;
+//Redeclare the name variable.
+var name;
+//initialise age variable to 50.
+age = 50;
+//Reinitialise name variable to rino monica nicholus
+name = "rino monica nicholus";
+/*
+Output My name is rino monica nicholus and I am 50 years old to the webpage.
+*/
+document.write("My name is " +name +" "+ "and I am "+age+" "+" years old");
+
+//Declare a variable x.
+var x;
+//output variable x value to the webpage.
+document.write(x);
+//give x a data type undefined.
+var x = undefined;
+//Output the value of x to the console.
+console.log(x);
