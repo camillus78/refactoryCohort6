8e1030d431fe04cdaa6092064a49243cde642672
camillus78 <60211082+camillus78@users.noreply.github.com>
2020-09-25 09:24:31 -0700

    Create mulondo.js
    This contains the first assignment from ozzy

---
+++ mulondo.js
@@ -0,0 +1,56 @@
+//Declaring variable name.
+var name;
+//Assigning the variable name to ozzy zainab.
+name = " ozzy zainab"
+
+//Printing out the values stored in variable name.
+console.log(name);
+//Pinting a breakline. 
+console.log("<br/>");
+
+//Declaring variable name.
+var name;
+//Assigning name to ozzy emma otim.
+name = "ozzy emma otim "
+//Printing the values stored in the varriable name.
+console.log(name)
+
+//Declaring the variable name again.
+var name;
+//Assigning it to a string marrie jacob ozyy.
+name = "marrie jacob ozyy"
+//Printing the values stored in the variable name in the console.
+console.log(name)
+//Declaring a new variable name. 
+var name;
+//Assigning variable name to a string ozzy diane.
+name = " ozzy diane"
+//Printing the values stored in the variable name.
+console.log(name)
+//Assigning variable name to a string joanita paul ozzy.
+name = "joanita paul ozzy "
+//Printing the values stored in the varible name.
+console.log(name)
+//Declaring variable age.
+var age;
+//Declarong variable name.
+var name;
+//Assigning variable age to a number 50.
+age = 50;
+//Assigning the variable name to a string rino moica nicholus.
+name = "rino monica nicholus"
+/*
+Document.write comands the computer to write to the console
+in the console,the expected outcome is, My name is rino monica nicholas and I am 50 years old.
+
+*/
+document.write("My name is " +name +" "+ "and I am "+age+" "+" years old");
+
+//A variable named x is declared.
+var x;
+//Document.write comands the computer to print in the console.
+document.write(x);
+//Variable x is not assigned to any value.
+var x = undefined;
+//Prints the values stored in variable x which in this case is undefined.
+console.log(x);
