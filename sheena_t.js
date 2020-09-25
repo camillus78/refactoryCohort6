2aa0ace594c0cc0975850ef6c94e0e385cbaae57
Tushabe Sheena Leonah <turyasheena@gmail.com>
2020-09-25 10:08:42 +0300

    first assignment

---
+++ sheena_t.js
@@ -0,0 +1,67 @@
+
+var name;
+//Declare a variable as name.
+name = " ozzy zainab";
+//Initialize the variable name with the value ozzy zainab.
+
+console.log(name);
+//Output the result of the name which is ozzy zainab variable in the terminal. 
+
+
+console.log("<br/>");
+//Print <br/> on the terminal.
+
+
+var name;
+//Declare a variable as name.
+name = "ozzy emma otim ";
+//Intialize the variable name with the value ozzy emma otim.
+
+console.log(name);
+//Print the result of the variable name which is ozzy emma otim in the terminal.
+
+
+var name;
+//Declare variable name.
+name = "marrie jacob ozyy";
+//Initialize variable name with the value marrie jacob ozyy.
+
+console.log(name);
+//Output the value for name which is marrie jacob ozyy in the terminal.
+
+
+var name;
+//Declare variable name.
+name = " ozzy diane";
+//Initialize variable name with value ozzy diane.
+
+console.log(name);
+//Output the value for name which is ozzy diane in the terminal.
+
+
+name = "joanita paul ozzy ";
+//Initialize variable name with the value joanita paul ozzy.
+console.log(name);
+//Output the value for name which is joanita paul ozzy in the terminal.
+
+
+var age;
+//Declare a variable age.
+var name;
+//Declare a variable name.
+age = 50;
+//Initialize variable age with the value 50.
+name = "rino monica nicholus";
+/*
+Initialize variable name with the value rino monica nicholus.
+*/
+document.write("My name is " +name +" "+ "and I am "+age+" "+" years old");
+//Print out My name is rino monica nicholus and I am 50 years old but in html pages because of the document(write). 
+var x;
+//Declare variable x.
+document.write(x);
+//Print the value of x which is undefined but still in the html page necause of the document(write).
+var x = undefined;
+//Initialize variable x with the data type undefined.
+console.log(x);
+//Print out the value of x which is undefined.
