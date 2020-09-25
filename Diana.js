43e49aca866314aa733dad722512265c96bb29fe
apolatdiana <61685214+apolatdiana@users.noreply.github.com>
2020-09-25 14:40:18 +0300

    Diana.js

---
+++ Diana.js
@@ -0,0 +1,82 @@
+// declares a variable called name.
+var name;
+
+// initialises the value "ozzy zainab"( which is of data type string)
+// and stores it in the above variable "name".
+name = " ozzy zainab";
+
+// Enables the value stored in the variable
+// "name" to be display which is "ozzy zainab"
+console.log(name);
+
+// Enables the computer to dislay the break tag of the element br  as"<br/>"
+console.log("<br/>");
+
+//Declares the variable called name 
+var name;
+
+// a signs a value "ozzy emma otim"( of data type string) to the variable "name"
+name = "ozzy emma otim ";
+
+// Enables the value stored in the variable called
+// "name" to be display which is "ozzy emma otim" in the terminal
+console.log(name);
+
+//Declares a variable called name
+var name;
+
+// Assigns a string value "marrie jacob ozyy" to the declared variable called name
+name = "marrie jacob ozyy";
+
+//Enables the value stored in the variable called
+// "name" to be display which is "marrie jacob ozyy" in the terminal
+console.log(name);
+
+//Declares a variable called name
+var name;
+
+//Assigns a string value "ozzy diane" to the declared variable called name
+name = " ozzy diane";
+
+//Enables the value stored in the variable called
+// "name" to be display which is "ozzy diane" in the terminal
+console.log(name);
+
+//Assigns a string value "joanita paul ozzy" to the declared variable called name
+name = "joanita paul ozzy ";
+
+//Enables the value stored in the variable called
+// "name" to be display which is "joanita paul ozzy" in the terminal
+console.log(name);
+
+//Declares a variable called age
+var age;
+
+//Declares a variable called name
+var name;
+
+//Assigns a number 50, to the declared variable called age
+age = 50;
+
+//Assigns a string value "rino monica nicholus" to the declared variable called name
+name = "rino monica nicholus";
+/*
+"document.write" is used in a function and it helps to display the Values of 
+name ( "rino monica nicholus") and age(50) including the added strings in its braces.
+*/
+document.write("My name is " +name +" "+ "and I am "+age+" "+" years old");
+
+//Declares a variable called "x".
+var x;
+
+// Is to display the value of "x", but "x" was not initialised(ie. is not defined)
+//document.write" is used in a function. 
+document.write(x);
+
+// Declares a variable "x" and initialises it will a variable "undefined" is 
+//not declared anywhere. 
+var x = undefined;
+
+//Enables the value stored in the variable called
+// "x" to be display which should be a value in the variable "undifined"
+console.log(x);
