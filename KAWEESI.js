bf65653e97920a477c608d9571ce7e183a3f9282
kaweesi <red@gmail>
2020-09-25 11:42:55 +0300

    aadded comments to assignement

---
+++ KAWEESI.js
@@ -0,0 +1,56 @@
+//Declaring name variable.
+var name;
+//Initiallising the variable name with value: ozzy zainab.
+name = " ozzy zainab";
+
+//Outputting the value of variable name as:ozzy zainab.
+console.log(name);
+//Outputting <br/> 
+console.log("<br/>");
+//Declaring variable as name.
+
+var name;
+//Initiallising variable name with value ozzy emma otim.
+name = "ozzy emma otim ";
+
+//Outputting the value of variable name as ozzy emma otim.
+console.log(name);
+
+//Declaring the name variable.
+var name;
+//Initiallising the variable name with value marrie jacob ozyy.
+name = "marrie jacob ozyy";
+//Outputting the value of name marrie jacob ozyy. 
+console.log(name);
+//Declaring name variable.
+var name;
+//Initiallising name variable with value ozzy diane.
+name = " ozzy diane";
+//Outputting ozzy diane.
+console.log(name);
+//Declaring name variable with value joanita paul ozzy.
+name = "joanita paul ozzy ";
+//outputting the value for variable name;joanita paul ozzy.
+console.log(name);
+//Declaring age variable with no value.
+var age;
+//Declaring name variable with no value.
+var name;
+//Initiallising age variable with value of 50,age stores 50.
+age = 50;
+//Initiallising name variable with value rino monica nicholus.
+name = "rino monica nicholus";
+
+
+//A function that requires to write my name and age.
+
+document.write("My name is " +name +" "+ "and I am "+age+" "+" years old");
+
+//Declaring x as variable with no value.
+var x;
+//
+document.write(x);
+//Initiallising x variable with un defined value.
+var x = undefined;
+//outputting value for variable x.
+console.log(x);
