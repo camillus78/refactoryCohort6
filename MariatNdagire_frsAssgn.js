31795fd227bedffa2c789d2a1d1a85eaa67e0b50
Mariat28 <38540447+Mariat28@users.noreply.github.com>
2020-09-25 11:18:53 +0300

    Create MariatNdagire_frsAssgn.js
     My First Js Assignment

---
+++ MariatNdagire_frsAssgn.js
@@ -0,0 +1,56 @@
+//declare a variable called name
+var name;
+//Assign a value to  the name variable 
+name = " ozzy zainab";
+
+//Print out the contents of the name variable on the console
+console.log(name);
+// Print out  the string <br/> on the console
+console.log("<br/>");
+//Declare a variable called name
+
+var name;
+//Assign a value to the name variable 
+name = "ozzy emma otim ";
+
+//Print out the contents of the name  variable on the console
+console.log(name);
+
+//declare a variable called name
+var name;
+//assign a value to  the name variable
+name = "marrie jacob ozyy";
+//Print out value of name to the console
+console.log(name);
+//declare a variable called name
+var name;
+//assign a  string  ozzy diane to the name variable
+name = " ozzy diane";
+//print out the value of variable name to the console
+console.log(name);
+//reassign a new value to the name variable
+name = "joanita paul ozzy ";
+//print out the new contents of the name variable to the console
+console.log(name);
+//declare a variable called age
+var age;
+//declare a variable called name
+var name;
+//assign an integer value 50 to the  age variable
+age = 50;
+//assign a string value rino monica nicholus to the name variable
+name = "rino monica nicholus";
+
+//print out the string, My name is rino monica nicholus and I am 50 years old in a html page
+
+
+document.write("My name is " + name + " " + "and I am " + age + " " + " years old");
+
+//declare a variable x
+var x;
+//print out variable x on the console
+document.write(x);
+//initialise x to undefined
+var x = undefined;
+//print out the value of x
+console.log(x);
