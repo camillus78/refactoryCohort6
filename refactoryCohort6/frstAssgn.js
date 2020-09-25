ead101b1e94d1b5732eeb3da82648038b38b6db4
Conrad Waako <eliezerconrad256@gmail.com>
2020-09-25 18:18:03 +0300

    ConradW

--- refactoryCohort6/frstAssgn.js
+++ refactoryCohort6/frstAssgn.js
@@ -1,53 +1,55 @@
-//
+//declaring variable name
 var name;
-//
+//assigning it a string value
 name = " ozzy zainab";
-
-//
+//this outputs the current value of the variable (name)
 console.log(name);
-// 
+//would create a space between the the first line and next line but in htis case will just print out "</br>"
 console.log("<br/>");
-//
 
+//declaring name again
 var name;
-//
+//reassigning the variable to another value
 name = "ozzy emma otim ";
-
-//
+//go to line 5
 console.log(name);
 
-//
+//go to line 11
 var name;
-//
+//go to line 13
 name = "marrie jacob ozyy";
-//
+//go to line 5
 console.log(name);
-//
-var name;
-//
-name = " ozzy diane";
-//
-console.log(name);
-//
-name = "joanita paul ozzy ";
-//
-console.log(name);
-//
-var age;
-//
-var name;
-//
-age = 50;
-//
-name = "rino monica nicholus";
-/*
 
+//go to line 11
+var name;
+//go to line 13
+name = " ozzy diane";
+//go to line 5
+console.log(name);
+
+//go to line 11
+name = "joanita paul ozzy ";
+//go to line 6
+console.log(name);
+
+//declare variable age
+var age;
+//go to line 11
+var name;
+//assigning the age variable a value
+age = 50;
+//assigning the name variable another value
+name = "rino monica nicholus";
+/*prints "My name is  rino monica nicholus and I am 50 years old"
+ * "+" signs concatenate the name and age variable in this case
+only good for testing purposes as it can override your html thats loaded and delete it
 */
 document.write("My name is " +name +" "+ "and I am "+age+" "+" years old");
 
-//
+//declares variable x
 var x;
-//
+//prints error as x has been declared but not assigned a value
 document.write(x);
 //
 var x = undefined;
