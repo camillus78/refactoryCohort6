3f7a47fbaa6448cb5bc8b7dbde27ee244081304f
OpiniIsaac <65159215+OpiniIsaac@users.noreply.github.com>
2020-09-25 10:59:23 +0300

    Add files via upload

---
+++ opini/frstAssgn.js
@@ -0,0 +1,55 @@
+//declaring a variable name 
+var name;
+//intialising a value which is a string has been assigned to varaible name
+name = " ozzy zainab"
+
+//decearing a varaible name  to be printed
+console.log(name);
+// 
+console.log("<br/>");
+//declaring a variable name 
+
+var name;
+// intialising varaible name stores a value which is string with ozzy zainab
+name = "ozzy emma otim "
+//print/output varaible defined as name with a string ozzy emma otim
+console.log(name)
+//declaring a variable name 
+
+var name;
+//intaialising a value which is a string has been assigned to varaible name
+name = "marrie jacob ozyy"
+//decearing a varaible name  to be printed
+console.log(name)
+//declaring a variable name 
+var name;
+//a value which is a string has been assigned to varaible name
+name = " ozzy diane"
+//decearing a varaible name  to be printed
+console.log(name)
+//print/output varaible defined as name with a string
+name = "joanita paul ozzy "
+//the string from the varaible name will be output/printed 
+console.log(name)
+//declaring a variable name 
+var age;
+//declaring a variable name 
+var name;
+//a value which is a numic number has been assigned to varaible name
+age = 50;
+//a value which is a string has been assigned to varaible name
+name = "rino monica nicholus"
+/*intialising text to be printed with the strings of text typed with the diifrent data typpes
+in the variables
+
+*/
+document.write("My name is " +name +" "+ "and I am "+age+" "+" years old");
+
+//declaring a variable name 
+var x;
+//intialising the varaible x to be printed
+document.write(x);
+//a varialble has not been assigned a value
+var x = undefined;
+//intialising an empty varaible to be printed
+console.log(x);
