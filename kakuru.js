1f6d67661f9dc531436654e25c6e322a3b42d15c
kaks <esaukaks@gmail.com>
2020-09-25 09:25:31 -0700

    changes

---
+++ kakuru.js
@@ -0,0 +1,54 @@
+//Declaration of the varible.
+var name;
+//Assigning the  variable "name" with a value.
+name = " ozzy zainab";
+
+//Function to show the product of the variable.
+console.log(name);
+//function that shows the product of the undeclared variable "<br/>".
+console.log("<br/>");
+//Re-declaration of the variable name.
+var name;
+//Variable re-assigned a string value 'OZZY EMMA OTIM'.
+name = "ozzy emma otim ";
+
+//Function to get the output of the variable.
+console.log(name);
+
+//Re-Declaration of a variable. 
+var name;
+//Re-assigning a variable with a string(value).
+name = "marrie jacob ozyy";
+//Function to output the vlaue of the variable.
+console.log(name);
+//Another declaration of the variable.
+var name;
+//Another assignment of the variable 'name'.
+name = " ozzy diane";
+//Function of the output of the variable.
+console.log(name);
+//A declartion has been assigned a value(string) "joanita paul ozzy"'
+name = "joanita paul ozzy ";
+//Function of the declared variable "name".
+console.log(name);
+//A variable 'age' has been declared but not assigned a value.
+var age;
+//a variable 'name' has been declared but not given a value.
+var name;
+//The variable "age" has been assigned a value.
+age = 50;
+//the variable has been assigned a value "rino monica nicholus"
+name = "rino monica nicholus";
+/*
+A way to putout the values of the initialised declared variables.
+*/
+document.write("My name is " + name + " " + "and I am " + age + " " + " years old");
+
+//The variable has been declared.
+var x;
+//A way to work out the value of x.
+document.write(x);
+//Variable x has been assigned the value undefined.
+var x = undefined;
+//Function to get the value x.
+console.log(x);
