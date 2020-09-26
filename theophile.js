76276ba913f0f2638c9b64a6ed77ea049171f34a
Theophile Sugira <nyembless@gmail.com>
2020-09-26 15:31:35 +0300

    comments on codes

---
+++ theophile.js
@@ -0,0 +1,56 @@
+// This statement declare a variable called name.The same variable name has no value assigned to it.
+var name;
+
+// This statement initializes  the declared variable name with a value "Ozzy Zainab"
+name = " ozzy zainab"
+
+// This operation is used to write out the results. In this statement, it print out the value name on the terminal
+console.log(name);
+// This statement output the string <br/> into the console
+console.log("<br/>");
+//This statement declares a variable called name which is undefined
+
+var name;
+//This statement initializes  the declared variable name with a value "Ozzy emma otim"
+name = "ozzy emma otim "
+//This statement print out the value name on the terminal
+console.log(name)
+//This statement declare a variable called name.The same variable name has no value assigned to it.
+
+var name;
+// Statement inizializing a variable name with a string " marie jacob ozzy"
+name = "marrie jacob ozyy"
+//This statement print out the value name on the terminal.
+console.log(name)
+//This statement declares a variable called name with an undefined value
+var name;
+//Statement initializing a name with a string called" ozzy diane"
+name = " ozzy diane"
+//This statement print out the value name on the terminal.
+console.log(name)
+//Statement inizializing a variable name with a string " joanita paul ozzy"
+name = "joanita paul ozzy "
+//This statement print out the value name on the terminal
+console.log(name)
+// This statement declare a variable called agewith an undefine value.
+var age;
+//This statement declares a variable named without a value.
+var name;
+//Statement initialising age and give it a value 50.
+age = 50;
+//Statement initializing a name with a string called" rino monica nicholus"
+name = "rino monica nicholus"
+/*
+This statement defined on the document write a string  that will have the following result:
+    " My name is rino monica nicholus and i am 50 years old"
+*/
+document.write("My name is " +name +" "+ "and I am "+age+" "+" years old");
+
+// This statement declare a variable x which does not have a value.
+var x;
+//This statement declare a variable called x which is undefined to be added to the document.
+document.write(x);
+// This statement declare a variable called x and initialize it  with a value undefined
+var x = undefined;
+//This statement print out the value x on the terminal
+console.log(x); 
